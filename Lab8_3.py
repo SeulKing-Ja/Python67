9df5e6a25c80ae1033d426f3cffef28c14fe0318
SeulKing-Ja <95630899+SeulKing-Ja@users.noreply.github.com>
2024-09-22 01:38:20 +0700

    UPDATE

--- Lab8_3.py
+++ Lab8_3.py
@@ -7,7 +7,31 @@ board = [['*', '*', '*', '*', '*'],
 
 
 #--------------Function--------------#
+def createBoard(board):
+    for i,e in enumerate(board):
+        for j in e:
+            print(j, end="")
+        print()
 
-
-
+def scan(board):
+    scan_text = 'KMITL'
+    lst = []
+    for rows in range(5):
+        for cols in range(5):
+            if board[rows][cols] in scan_text:
+                lst.append([board[rows][cols], rows+1, cols+1])
+    return lst 
+        
+def order_text(board): 
+    lst = scan(board)
+    res = []
+    to_addlst = []
+    i = 0
+    max = len(lst)
+    while True:
+        for index,items in enumerate(lst):
+            pass
+        break
+    print(to_addlst)
 #--------------Main--------------#
+print(order_text(board))
