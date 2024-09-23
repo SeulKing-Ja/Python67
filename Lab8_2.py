15ee4b3cc6309d57abd0dba953805deb80ddeb08
SeulKing-Ja <95630899+SeulKing-Ja@users.noreply.github.com>
2024-09-23 16:41:37 +0700

    Update8_2

--- Lab8_2.py
+++ Lab8_2.py
@@ -1,1 +1,20 @@
-#Create Board
+#Function
+def create_board():
+    board = []
+    size = 10
+    for i in range(size): 
+        for j in range(size):
+            board.append('0')
+    return board
+
+def write_board(board):
+    for i in range(len(board)):
+        if i % 10 == 0:
+            print()
+        print(board[i], end='')
+        
+
+
+#Main   
+board = create_board()
+write_board(board)
